b6271dcdf37cc97a15f136ef44238f74f8d1537f
Joez <Joez@xogrp.com>
2016-10-24 01:59:36 +0800

    rx test

--- rxlearn/src/main/java/com/example/RxBean.java
+++ rxlearn/src/main/java/com/example/RxBean.java
@@ -2,10 +2,14 @@ package com.example;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import rx.Observable;
 import rx.Observer;
 import rx.Subscriber;
+import rx.Subscription;
+import rx.functions.Func0;
+import rx.functions.Func1;
 
 /**
  * Created by Administrator on 2016/10/22 0022.
@@ -14,7 +18,7 @@ public class RxBean {
 
     private List<String> generateCap() {
         List<String> list = new ArrayList<>();
-        list.add("A");
+        list.add("D");
         list.add("B");
         list.add("C");
         return list;
@@ -26,6 +30,23 @@ public class RxBean {
         list.add("Y");
         list.add("Z");
         return list;
+    }
+
+    private Observable<String> getObservableList() {
+        return Observable.create(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                List<String> list = generateCap();
+                for (String s : list) {
+                    if(!subscriber.isUnsubscribed()) {
+                        subscriber.onNext(s);
+                    }
+                }
+                if (!subscriber.isUnsubscribed()) {
+                    subscriber.onCompleted();
+                }
+            }
+        });
     }
 
     private void print(String string) {
@@ -53,4 +74,184 @@ public class RxBean {
         });
     }
 
+    public void testSubscriber() {
+        getObservableList().toSortedList().subscribe(new Observer<List<String>>() {
+            @Override
+            public void onCompleted() {
+                print("onCompleted");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(List<String> list) {
+                for (String s : list) {
+                    print("---"+s);
+                }
+            }
+        });
+    }
+
+    public void testFrom() {
+        Observable.from(generateEnd()).subscribe(new Observer<String>() {
+            @Override
+            public void onCompleted() {
+                print("complete");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(String s) {
+                print(s);
+            }
+        });
+    }
+
+    public void testJoin() {
+        Observable.just("A","B","C").repeat(2)
+                .subscribe(new Observer<String>() {
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onNext(String s) {
+                        print(s);
+                    }
+                });
+    }
+    private String i = null;
+    public void testDefer() {
+        i ="A";
+        Observable<String> ob=Observable.defer(new Func0<Observable<String>>() {
+            @Override
+            public Observable<String> call() {
+                return Observable.from(new String[]{i});
+            }
+        });
+        i ="B";
+        ob.subscribe(new Observer<String>() {
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(String s) {
+                print(s);
+            }
+        });
+    }
+
+    public void testRange() {
+        Observable.range(3,2)
+                .subscribe(new Observer<Integer>() {
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        print("next "+integer);
+                    }
+                });
+
+    }
+
+    public void testInterval() {
+        Subscription sup = Observable.interval(3, 4,TimeUnit.SECONDS)
+                .subscribe(new Observer<Long>() {
+                    @Override
+                    public void onCompleted() {
+                        print("completed");
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        print(e.getMessage());
+                    }
+
+                    @Override
+                    public void onNext(Long aLong) {
+                        print("-----");
+                    }
+                });
+
+    }
+
+    public void testTimer() {
+        Observable.timer(2,TimeUnit.SECONDS)
+                .subscribe(new Observer<Long>() {
+                    @Override
+                    public void onCompleted() {
+                        print("complete");
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onNext(Long aLong) {
+                        print("-------------"+aLong);
+                    }
+                });
+    }
+
+    public void testFilter() {
+        Observable.from(generateEnd()).repeat(3)
+//                .timeout(1,TimeUnit.SECONDS)
+//               .firstOrDefault("C")
+//                .distinctUntilChanged()
+//                .distinct()
+//                .take(1)
+//                .takeLast(1)
+//                .filter(new Func1<String, Boolean>() {
+//                    @Override
+//                    public Boolean call(String s) {
+//                        return !s.equals("Z");
+//                    }
+//                })
+                .subscribe(new Observer<String>() {
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(String s) {
+                print(s);
+            }
+        });
+    }
 }
--- rxlearn/src/main/java/com/example/RxTest.java
+++ rxlearn/src/main/java/com/example/RxTest.java
@@ -4,8 +4,15 @@ public class RxTest {
 
     public static void main(String[] args) {
         RxBean rx = new RxBean();
-        rx.testMerge();
+//        rx.testMerge();
+//        rx.testSubscriber();
+//        rx.testFrom();
+//        rx.testJoin();
+//        rx.testDefer();
+//        rx.testRange();
+//        rx.testInterval();
+//        rx.testTimer();
+        rx.testFilter();
     }
-
 
 }
